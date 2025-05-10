6b7263c6b874c26c54774d216086cc058f90ccb2
fescii <isfescii@gmail.com>
2025-05-10 05:19:51 +0300

    Add 'Bland' navigation section and update styles

--- js/app.js
+++ js/app.js
@@ -342,6 +342,7 @@ export default class AppMain extends HTMLElement {
       ${this.getSheetsNav()}
       ${this.getCacheNav()}
       ${this.getPricingNav()}
+      ${this.getBlandNav()}
       ${this.getDocsNav()}
       ${this.getUserNav()}
     `;
@@ -458,6 +459,41 @@ export default class AppMain extends HTMLElement {
             </li>
             <li class="test">
               <a href="/pricing/test"><span class="text">Test</span></a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    `;
+  }
+
+  getBlandNav = () => {
+    return /* html */`
+      <ul class="special nav">
+        <li class="bland">
+          <div class="link-section">
+            <span class="left">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="currentColor" fill="none">
+                <path d="M3 8.5H15C17.8284 8.5 19.2426 8.5 20.1213 9.37868C21 10.2574 21 11.6716 21 14.5V15.5C21 18.3284 21 19.7426 20.1213 20.6213C19.2426 21.5 17.8284 21.5 15 21.5H9C6.17157 21.5 4.75736 21.5 3.87868 20.6213C3 19.7426 3 18.3284 3 15.5V8.5Z" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M15 8.49833V4.1103C15 3.22096 14.279 2.5 13.3897 2.5C13.1336 2.5 12.8812 2.56108 12.6534 2.67818L3.7623 7.24927C3.29424 7.48991 3 7.97203 3 8.49833" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M17.5 15.5C17.7761 15.5 18 15.2761 18 15C18 14.7239 17.7761 14.5 17.5 14.5M17.5 15.5C17.2239 15.5 17 15.2761 17 15C17 14.7239 17.2239 14.5 17.5 14.5M17.5 15.5V14.5" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+              <span class="text">Bland</span>
+            </span>
+            <span class="right">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="currentColor" fill="none">
+                <path d="M18 9.00005C18 9.00005 13.5811 15 12 15C10.4188 15 6 9 6 9" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+            </span>
+          </div>
+          <ul class="dropdown">
+            <li class="recent">
+              <a href="/bland/recent"><span class="text">Recent</span></a>
+            </li>
+            <li class="failed">
+              <a href="/bland/failed"><span class="text">Failed</span></a>
+            </li>
+            <li class="all">
+              <a href="/bland/all"><span class="text">All Calls</span></a>
             </li>
           </ul>
         </li>
@@ -1052,7 +1088,7 @@ export default class AppMain extends HTMLElement {
           font-size: 0.9rem;
           display: inline-block;
           padding: 0 0 0 5px;
-          color: var(--accent-color);
+          color: var(--alt-color);
           font-family: var(--font-text), sans-serif;
         }
 
@@ -1070,8 +1106,9 @@ export default class AppMain extends HTMLElement {
 
         footer.footer > ul.links > li {
           padding: 0;
-          margin: 0;
-          list-style-type: none;
+          margin: 0 0 0 12px;
+          list-style-type: default;
+          color: var(--gray-color);
         }
 
         footer.footer > ul.links > li > a {
